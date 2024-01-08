e4270b06bed716990a35641464b2edcda7c26ce8
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-01-08 14:47:41 -0800

    Update requirements in setup script

--- setup.cmd
+++ setup.cmd
@@ -1,10 +1,21 @@
 @echo off
 setlocal EnableExtensions
-call "%~dp0.venv\Scripts\activate.bat"
-call py -3 -m pip install --user distutils
-call py -3 -m pip uninstall -y pipywin32
-call py -3 -m pip uninstall -y pywin32
-call py -3 -m pip install --user pywin32>=306
-if exist "%~dp0Scripts\pywin32_postinstall.py" (
-    call python "%~dp0Scripts\pywin32_postinstall.py" -install
-)
+    call "%~dp0.venv\Scripts\activate.bat"
+    call py -3 -m pip uninstall -y pipywin32 pywin32
+    call py -3 -m pip install --user -e ".[dev,test,types,ci,lint,pip,release]"
+
+    if exist "%~dp0Scripts\pywin32_postinstall.py" (
+        call py -3 "%~dp0Scripts\pywin32_postinstall.py" -install
+    )
+
+    call "%~dp0.venv\Scripts\deactivate.bat"
+    pipx run --spec pip-tools pip-compile ^
+        --all-extras --build-isolation --verbose ^
+        -o "%~dp0requirements.txt" ^
+        "%~dp0pyproject.toml"
+
+    pipx run --spec pip-tools pip-compile ^
+        --extras=ci --build-isolation --verbose ^
+        -o "%~dp0requirements\ci.txt" ^
+        "%~dp0pyproject.toml"
+endlocal & exit /b %ERRORLEVEL%
