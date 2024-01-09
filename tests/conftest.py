1023b610a4e75c6ac99e9eb57c9be1145e133168
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-01-09 12:32:49 -0800

    nit: Reformatted conftest
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ import pytest
 @pytest.fixture(name="test_dir")
 def fixture_test_output_directory(tmp_path: Path) -> Generator[str, None, None]:
     """Destination directory for package tests."""
-    output_path =tmp_path.joinpath("oschmod")
+    output_path = tmp_path.joinpath("oschmod")
     output_path.mkdir(exist_ok=False)
     yield str(output_path)
     shutil.rmtree(str(output_path))
