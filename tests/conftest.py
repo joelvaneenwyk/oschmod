e6eb982cf4a7e5eb46fd89cc44578f8dc48b5734
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-01-09 13:44:00 -0800

    chore: VSCode settings
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,8 @@ from typing import Generator
 
 import pytest
 
+from oschmod import set_mode_recursive
+
 
 @pytest.fixture(name="test_dir")
 def fixture_test_output_directory(tmp_path: Path) -> Generator[str, None, None]:
@@ -13,4 +15,5 @@ def fixture_test_output_directory(tmp_path: Path) -> Generator[str, None, None]:
     output_path = tmp_path.joinpath("oschmod")
     output_path.mkdir(exist_ok=False)
     yield str(output_path)
-    shutil.rmtree(str(output_path))
+    set_mode_recursive(output_path, 0o777)
+    shutil.rmtree(str(output_path), ignore_errors=True)
